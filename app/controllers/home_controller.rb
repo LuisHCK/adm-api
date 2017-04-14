e49933d79c55d4e411d0cc5bfc2a9ad85ee4ecdf
luishck <luisjcenteno@live.com>
2017-04-14 00:02:27 -0600

    Check cash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,16 @@
 class HomeController < ApplicationController
 	def index
+		total = 0
 		@sales_count = Sale.where(user_id: @current_user.id).count()
 		@services_count = Service.where(user_id: @current_user.id).count()
 		@cash = Cash.find_by(user_id: @current_user.id, date_close: nil)
+		
+		#Check cash
+		if @cash
+			total = @cash.total
+		end
 
-		render json: {sales: @sales_count, services: @services_count, cash: @cash.total}
+
+		render json: {sales: @sales_count, services: @services_count, cash: total}
 	end
 end
